79a1d48219da44da688ae559acb838adf825c80e
sungju-kim <wnwn0719@gmail.com>
2022-07-13 17:50:41 +0900

    Feat: StyleCell 구현
    #4

---
+++ musinsa/musinsa/Source/Present/Cell/StyleCell.swift
@@ -0,0 +1,46 @@
+//
+//  StyleCell.swift
+//  musinsa
+//
+//  Created by dale on 2022/07/13.
+//
+
+import UIKit
+import SnapKit
+
+final class StyleCell: UICollectionViewCell {
+    static var identifier: String {
+        return "\(self)"
+    }
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        layoutImageView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        layoutImageView()
+    }
+}
+
+// MARK: - Layout Section
+
+private extension StyleCell {
+    func layoutImageView() {
+        addSubview(imageView)
+
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
