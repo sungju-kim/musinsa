1daedf7fdb21586a90c0335e8e57a9ba5192daca
sungju-kim <wnwn0719@gmail.com>
2022-07-14 14:33:13 +0900

    Feat: MainViewController에 collectionView 추가
    #8

--- musinsa/musinsa/Source/Present/MainViewController.swift
+++ musinsa/musinsa/Source/Present/MainViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     private var domain: Products?
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: LayoutFactory.create())
+        collectionView.register(GoodsCell.self, forCellWithReuseIdentifier: GoodsCell.identifier)
+        collectionView.register(BannerCell.self, forCellWithReuseIdentifier: BannerCell.identifier)
+        collectionView.register(StyleCell.self, forCellWithReuseIdentifier: StyleCell.identifier)
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        layoutCollectionView()
+    }
+}
+
+// MARK: - Layout Section
+
+private extension MainViewController {
+    func layoutCollectionView() {
+        view.addSubview(collectionView)
+
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
 
